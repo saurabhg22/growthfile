4e93254b54f2529a1775cdb9b4d83380cc606268
Saurabh Gupta <saurabhg22596@gmail.com>
2017-11-01 12:42:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public"));
 
-mongoose.connect("mongodb://localhost:27017/growthfile", function(err){
+mongoose.connect("mongodb://saurabh:grow1234@cluster0-shard-00-00-mydlt.mongodb.net:27017,cluster0-shard-00-01-mydlt.mongodb.net:27017,cluster0-shard-00-02-mydlt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin/growthfile", function(err){
     if(err){
         console.log("Not connected to the database!!!");
     }
